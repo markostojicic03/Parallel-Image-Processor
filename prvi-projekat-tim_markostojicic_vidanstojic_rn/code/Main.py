3dac759f0f40aa5da914aee7c00b2eef3f8c16c5
markostojicic03 <mstojicic12923rn@raf.rs>
2024-10-24 22:11:20 +0200

    Opis

--- prvi-projekat-tim_markostojicic_vidanstojic_rn/code/Main.py
+++ prvi-projekat-tim_markostojicic_vidanstojic_rn/code/Main.py
@@ -1,1 +1,5 @@
+# Sav kod treba da bude u jednom .py fajlu. (Mi cemo pisati sav kod u Main.py)
+# Potrebno je uključiti najmanje jedan primer json fajla i jednu sliku u repozitorijum.
+# Rok za predaju je 14.11.2024 do kraja dana.
+
 print('Hello world!')
